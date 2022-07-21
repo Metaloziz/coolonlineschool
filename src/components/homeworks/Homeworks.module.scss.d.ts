30d00198e45bb20f0dbace4e0ae3230f0266b4cb
Serjge <serjge@yandex.ru>
2022-07-21 12:16:35 +0300

    feat: add title olympiads in Homeworks

--- src/components/homeworks/Homeworks.module.scss.d.ts
+++ src/components/homeworks/Homeworks.module.scss.d.ts
@@ -1,4 +1,6 @@
 export const button: string;
 export const container: string;
 export const homeworks: string;
+export const olympics: string;
 export const panel: string;
+export const title: string;
