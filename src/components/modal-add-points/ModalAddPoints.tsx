a35c55d187b394e1c431c75e23c88db6abd7a136
Serjge <serjge@yandex.ru>
2022-07-22 15:55:30 +0300

    feat: creat ModalAddPoints & hook useModal

---
+++ src/components/modal-add-points/ModalAddPoints.tsx
@@ -0,0 +1,61 @@
+import { ChangeEvent, ReactElement, useState } from 'react';
+
+import { ButtonColorThemes } from '@app/enums';
+import { Button, CustomSelect, ModalBasic, TextField } from '@components';
+import { ADD_POINTS_OPTIONS_SELECT } from '@mock/modal';
+
+import styles from './ModalAddPoints.module.scss';
+
+interface ModalAddPointsProps {
+  isActive: boolean;
+  closeModal: () => void;
+  studentsName: string;
+}
+
+const INITIAL_POINT = '1';
+
+export const ModalAddPoints = ({
+  isActive,
+  closeModal,
+  studentsName,
+}: ModalAddPointsProps): ReactElement => {
+  const [points, setPoints] = useState<string>(INITIAL_POINT);
+
+  const onPointsChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { value } = e.currentTarget;
+
+    if (!Number.isFinite(Number(value))) {
+      return;
+    }
+    setPoints(value);
+  };
+
+  return (
+    <ModalBasic visibility={isActive} changeVisibility={closeModal} className={styles.container}>
+      <p className={styles.title}>
+        Баллы для ученика: <span>{studentsName}</span>
+      </p>
+      <div className={styles.wrapper}>
+        <p>Баллы</p>
+        <TextField
+          type="text"
+          value={points}
+          onChange={onPointsChange}
+          className={styles.textField}
+        />
+        <p>Выданы за:</p>
+        <CustomSelect
+          className={styles.select}
+          options={ADD_POINTS_OPTIONS_SELECT}
+          placeholder=""
+        />
+        <Button type="button" colorTheme={ButtonColorThemes.red}>
+          Сохранить
+        </Button>
+      </div>
+      <Button className={styles.button} type="button" colorTheme={ButtonColorThemes.blueGradient}>
+        Изменить
+      </Button>
+    </ModalBasic>
+  );
+};
---
+++ src/hooks/useModal.ts
@@ -0,0 +1,19 @@
+import { useCallback, useState } from 'react';
+
+export const useModal = (): [
+  isActiveModal: boolean,
+  openModal: () => void,
+  closeModal: () => void,
+] => {
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  const openModal = useCallback((): void => {
+    setIsActive(true);
+  }, []);
+
+  const closeModal = useCallback((): void => {
+    setIsActive(false);
+  }, []);
+
+  return [isActive, openModal, closeModal];
+};
